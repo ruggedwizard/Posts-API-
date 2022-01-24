bb30e97bf3e23e14406df74d7ee6c156cde3a27e
david isaac <davidisaacsurvive@gmail.com>
2022-01-24 21:55:11 +0100

    security updated to hide some details

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI, APIRouter
-
 from app import models
 from .database import engine
-
-
 from passlib.context import CryptContext
-import time
 from app.routers import post, user, auth
+from app.config import settings
 models.Base.metadata.create_all(bind=engine)
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 app = FastAPI()
+
+
+
 
 router = APIRouter()
 app.include_router(post.router)
