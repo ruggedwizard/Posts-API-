5ab3abb1a273abfea8fffe0c1b7c07b34366643e
david isaac <davidisaacsurvive@gmail.com>
2022-01-24 21:33:21 +0100

    removed psycopg2 connection method and left with sql alchemy

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ from app import models
 from .database import engine
 
 
-import psycopg2
-from psycopg2.extras import RealDictCursor
 from passlib.context import CryptContext
 import time
 from app.routers import post, user, auth
@@ -18,19 +16,6 @@ router = APIRouter()
 app.include_router(post.router)
 app.include_router(auth.router)
 app.include_router(user.router)
-
-
-
-while True:
-    try:
-        conn = psycopg2.connect(host="localhost", database="fastapi", user="postgres", password="admin", cursor_factory=RealDictCursor)
-        cusor = conn.cursor()
-        print("Database connection Sucessful!!")
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error: ", error)
-        time.sleep(6)
 
 
 @app.get("/")
